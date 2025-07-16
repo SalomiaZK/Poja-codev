1c691c29b846542a4c571155a890432637234e3b
SalomiaZK <salomia.razakarivony@gmail.com>
2025-07-16 09:50:05 +0300

    feat: log in test

--- src/test/java/com/hei/codev/endpoint/rest/controller/health/EnvControllerTest.java
+++ src/test/java/com/hei/codev/endpoint/rest/controller/health/EnvControllerTest.java
@@ -2,8 +2,10 @@ package com.hei.codev.endpoint.rest.controller.health;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+@Slf4j
 class EnvControllerTest {
   EnvController subject = new EnvController();
 
@@ -11,6 +13,7 @@ class EnvControllerTest {
   void preprod_env_test() {
     String expected = System.getenv("SECRET_KEY");
     String actual = subject.preprodEnv();
+    log.info("******** Running Env Test ******* ");
     assertEquals(expected, actual);
   }
 }
