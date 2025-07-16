8b7c19f9e2eaae85497b87830b00552ccd80dd0c
SalomiaZK <salomia.razakarivony@gmail.com>
2025-07-16 09:01:30 +0300

    feat: endpoint get /preprod-env

---
+++ src/main/java/com/hei/codev/endpoint/rest/controller/health/EnvController.java
@@ -0,0 +1,12 @@
+package com.hei.codev.endpoint.rest.controller.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EnvController {
+    @GetMapping("/preprod-env")
+    public String preprodEnv() {
+        return System.getenv("SECRET_KEY");
+    }
+}
